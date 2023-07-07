f19832c7b526ede4e4772af89fea6ae542c6ed12
Roman <sml3221@mail.ru>
2023-07-07 13:19:48 +0700

    hw 3,4

--- src/channel.py
+++ src/channel.py
@@ -10,6 +10,29 @@ class Channel:
     def __init__(self, channel_id: str) -> None:
         self.__channel_id = channel_id
 
+    def __str__(self):
+        return f'{self.title} ({self.url})'
+
+    def __add__(self, other):
+        return self.subscriber_count + other.subscriber_count
+
+    def __sub__(self, other):
+        return self.subscriber_count - other.subscriber_count
+
+    def __gt__(self, other):
+        return self.subscriber_count > other.subscriber_count
+
+    def __lt__(self, other):
+        return self.subscriber_count < other.subscriber_count
+
+    def __ge__(self, other):
+        return self.subscriber_count >= other.subscriber_count
+
+    def __le__(self, other):
+        return self.subscriber_count <= other.subscriber_count
+
+    def __eq__(self, other):
+        return self.subscriber_count == other.subscriber_count
     @property
     def channel_id(self):
         return self.__channel_id
@@ -29,7 +52,7 @@ class Channel:
 
     @property
     def subscriber_count(self):
-        return self.channel()['items'][0]['statistics']['subscriberCount']
+        return int(self.channel()['items'][0]['statistics']['subscriberCount'])
 
     @property
     def view_count(self):
---
+++ src/video.py
@@ -0,0 +1,39 @@
+from src.channel import Channel
+class Video:
+    def __init__(self, video_id: str) -> None:
+        self.__video_id = video_id
+
+    def __str__(self):
+        return f'{self.title}'
+
+    @property
+    def title(self):
+        return self.video()['items'][0]['snippet']['title']
+
+    @property
+    def url(self):
+        return f'https://www.youtube.com/watch?v={self.__video_id}'
+
+    @property
+    def view_count(self):
+        return self.video()['items'][0]['statistics']['viewCount']
+
+    @property
+    def like_count(self):
+        return self.video()['items'][0]['statistics']['likeCount']
+
+
+    def video(self):
+        video = Channel.get_service().videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                       id=self.__video_id
+                                       ).execute()
+        return video
+
+class PLVideo(Video):
+    def __init__(self, video_id, playlist_id):
+        super().__init__(video_id)
+        self.__playlist_id = playlist_id
+
+    @property
+    def playlist_id(self):
+        return self.playlist_id
